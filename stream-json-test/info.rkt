cd0b0c73e367cf866c7a8231a20e2fe52b6584d2
Sam Phillips <samdphillips@gmail.com>
2020-06-30 12:49:50 -0700

    Added minefield tests.

--- stream-json-test/info.rkt
+++ stream-json-test/info.rkt
@@ -3,5 +3,5 @@
 (define collection 'multi)
 (define version "0.1")
 (define pkg-authors '(samdphillips))
-(define deps '("base"))
+(define deps '("base" "stream-json-lib"))
 (define build-deps '())
---
+++ stream-json-test/json/stream/test/main.rkt
@@ -0,0 +1,101 @@
+#lang racket/base
+
+(require racket/file
+         racket/format
+         racket/runtime-path
+         racket/stream
+         racket/string
+         rackunit
+
+         json/stream)
+
+(provide test-file->list)
+
+(define-runtime-path test-files-path "minefield-tests")
+
+(define (json-filename? p)
+  (string-suffix? (path->string p) ".json"))
+
+(define (success-filename? p)
+  (define-values (_a b _c) (split-path p))
+  (string-prefix? (path->string b) "y_"))
+
+(define (failure-filename? p)
+  (define-values (_a b _c) (split-path p))
+  (string-prefix? (path->string b) "n_"))
+
+(define (test-files)
+  (for/list ([fname (in-directory test-files-path)]
+             #:when (json-filename? fname))
+    fname))
+
+(define (success-test-files)
+  (for/list ([fname (in-list (test-files))]
+             #:when (success-filename? fname))
+    fname))
+
+(define (make-success-tests parser-name parse-fn)
+  (make-test-suite
+    (~a parser-name " should successfully parse")
+    (for/list ([test-file-path (in-list (success-test-files))])
+      (define short-file-name
+        (let-values ([(a b c) (split-path test-file-path)]) b))
+      (test-suite
+        (path->string short-file-name)
+        (with-check-info (['file-contents (string-info (~.s (file->bytes test-file-path)))])
+          (test-not-exn
+            "parse file"
+            (lambda ()
+              (call-with-input-file test-file-path parse-fn))))))))
+
+(define (failure-test-files)
+  (for/list ([fname (in-list (test-files))]
+             #:when (failure-filename? fname))
+    fname))
+
+(define (make-failure-tests parser-name parse-fn)
+  (make-test-suite
+    (~a parser-name " should not successfully parse")
+    (for/list ([test-file-path (in-list (failure-test-files))])
+      (define short-file-name
+        (let-values ([(a b c) (split-path test-file-path)]) b))
+      (test-suite
+        (path->string short-file-name)
+        (with-check-info (['file-contents (string-info (~.s (file->bytes test-file-path)))])
+          (test-exn "parse file"
+                    exn:fail?
+                    (lambda ()
+                      (call-with-input-file test-file-path parse-fn))))))))
+
+(define (test-file->list fname)
+  (call-with-input-file (build-path test-files-path fname)
+    (lambda (inp)
+      (stream->list
+        (json-stream/well-formed
+          (port->json-stream inp))))))
+
+(module* test #f
+  (require racket/match
+           rackunit/text-ui
+           json)
+
+  (define (read-all-json-stream inp)
+    (define s (json-stream/well-formed
+                (port->json-stream inp)))
+    (define (read-all s)
+      (unless (stream-empty? s)
+        (read-all (stream-rest s))))
+    (read-all s))
+
+  (define-values (name parser)
+    (match (current-command-line-arguments)
+      [(vector "json") (values "json" read-json)]
+      [(vector "stream-json-lib")
+       (values "stream-json-lib" read-all-json-stream)]
+      [(vector) (values "stream-json-lib" read-all-json-stream)]))
+
+  (run-tests
+    (test-suite "minefield tests"
+                (make-success-tests name parser)
+                (make-failure-tests name parser))))
+
